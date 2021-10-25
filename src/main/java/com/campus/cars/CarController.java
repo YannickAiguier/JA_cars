bd794e83ad797ebd8b9e700b3c47f22a1715d0db
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-25 11:34:00 +0200

    Dev: mise en place de CarController

---
+++ src/main/java/com/campus/cars/CarController.java
@@ -0,0 +1,19 @@
+package com.campus.cars;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+	@GetMapping(value="/cars")
+	public String index() {
+		return "Liste de toutes les voitures";
+	}
+	
+	@GetMapping(value="/cars/{id}")
+	public String show(@PathVariable int id) {
+		return "La voiture avec l'id " + id;
+	}
+
+}
