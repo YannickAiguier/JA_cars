0bc5d66f3a764fc1aa4340605c60c56869edd3a4
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-25 16:18:29 +0200

    Dev: préparation des actions dans le contrôleur

--- src/main/java/com/campus/cars/CarController.java
+++ src/main/java/com/campus/cars/CarController.java
@@ -3,8 +3,11 @@ package com.campus.cars;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //annotation qui combine @Controller (gère les requêtes http) et @ResponseBody (lie la valeur de retour au corps de la réponse http, convertit l'objet renvoyé en JSON) 
@@ -17,15 +20,30 @@ public class CarController {
 	
 	// indique que la méthode répondra à une requête GET
 	// équivalent à @RequestMapping(method=RequestMethod.GET,value=...)
-	@GetMapping(value="/cars")
+	@GetMapping(value="/modeles")
 	public List<Car> index() {
 		return myCars.findAll();
 	}
 	
 	// indique que la méthode répondra à une requête GET
-	@GetMapping(value="/cars/{id}")
+	@GetMapping(value="/modeles/{id}")
 	public Car show(@PathVariable int id) {
 		return myCars.findCarById(id);
+	}
+	
+	@PostMapping(value="/modeles")
+	public Car addCar(Car car) {
+		return null;
+	}
+	
+	@PutMapping(value="/modeles/{id}")
+	public Car updateCar(@PathVariable int id, Car car) {
+		return null;
+	}
+	
+	@DeleteMapping(value="/modeles/{id}")
+	public Car deleteCar(@PathVariable int id) {
+		return null;
 	}
 
 }
