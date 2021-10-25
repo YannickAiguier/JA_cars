6d986de06ba5fa4352955f198a08c6023df5cc44
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-25 14:16:33 +0200

    Feat: version avec Thymeleaf

--- src/main/java/com/campus/cars/CarController.java
+++ src/main/java/com/campus/cars/CarController.java
@@ -3,31 +3,21 @@ package com.campus.cars;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 //annotation qui combine @Controller (gère les requêtes http) et @ResponseBody (lie la valeur de retour au corps de la réponse http, convertit l'objet renvoyé en JSON) 
 @Controller
 public class CarController {
 	
-	// instancie automatiquement un objet de la classe indiquée
-//	@Autowired
-//	private CarDao myCars;
 	private static List<Car> myCars = new ArrayList<Car>();
 	static {
 		myCars.add(new Car(1, new String("Peugeot"), new String("206"), new String("bleue")));
 		myCars.add(new Car(2, new String("Renault"), new String("Clio"), new String("rouge")));
 		myCars.add(new Car(3, new String("Ferrari"), new String("Testarossa"), new String("jaune")));
 	}
-	
-//	// injection via application.properties
-//	@Value("${welcome.message}")
-//	private String message;
 	
 	// indique que la méthode répondra à une requête GET
 	// équivalent à @RequestMapping(method=RequestMethod.GET,value=...)
