82f877ae948d2845cbe9ac45fe2d2bc77a01187a
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-26 14:57:32 +0200

    Test: tous les tests sont implémentés

--- src/test/java/com/campus/cars/CarsControllerTest.java
+++ src/test/java/com/campus/cars/CarsControllerTest.java
@@ -2,14 +2,14 @@ package com.campus.cars;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import io.swagger.models.HttpMethod;
 
 //@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,14 +26,18 @@ public class CarsControllerTest {
 
 	@Test
 	public void createModelTest() {
-		String body = this.restTemplate.postForObject("/modeles", "{\"marque\": \"Citroën\",\"modele\": \"Berlingo\",\"couleur\": \"vert\"}", String.class);
-		ResponseEntity<String> response = restTemplate.postForEntity("/modeles", "{\"marque\": \"Citroën\",\"modele\": \"Berlingo\",\"couleur\": \"vert\"}", String.class);
+		String body = this.restTemplate.postForObject("/modeles",
+				"{\"marque\": \"Citroën\",\"modele\": \"Berlingo\",\"couleur\": \"vert\"}", String.class);
+		ResponseEntity<String> response = restTemplate.postForEntity("/modeles",
+				"{\"marque\": \"Citroën\",\"modele\": \"Berlingo\",\"couleur\": \"vert\"}", String.class);
 		assertThat(response.getStatusCode().equals(HttpStatus.CREATED));
 	}
 
 	@Test
 	public void updateModelTest() {
-
+		ResponseEntity<String> response = restTemplate.postForEntity("/modeles",
+				"{\"marque\": \"Citroën\",\"modele\": \"Berlingo\",\"couleur\": \"beige\"}", String.class);
+		assertThat(response.getStatusCode().equals(HttpStatus.OK));
 	}
 
 	@Test
@@ -45,6 +49,9 @@ public class CarsControllerTest {
 
 	@Test
 	public void deleteModelTest() {
-
+		restTemplate.delete("/modeles/4");
+		String body = this.restTemplate.getForObject("/modeles", String.class);
+		String result = "[{\"id\":1,\"marque\":\"Peugeot\",\"modele\":\"206\",\"couleur\":\"bleue\"},{\"id\":2,\"marque\":\"Renault\",\"modele\":\"Clio\",\"couleur\":\"rouge\"},{\"id\":3,\"marque\":\"Ferrari\",\"modele\":\"Testarossa\",\"couleur\":\"jaune\"}]";
+		assertThat(body).isEqualTo(result);
 	}
 }
