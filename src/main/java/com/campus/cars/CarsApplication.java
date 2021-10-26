18de0f91e9ad247deee28adde89f68de37899826
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-26 11:03:51 +0200

    Dev: configuration de swagger et ajout des annotations pour la documentation de l'API

--- src/main/java/com/campus/cars/CarsApplication.java
+++ src/main/java/com/campus/cars/CarsApplication.java
@@ -3,7 +3,10 @@ package com.campus.cars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CarsApplication {
 
 	public static void main(String[] args) {
