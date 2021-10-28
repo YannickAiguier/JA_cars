e3e6d44b03115a07d4b561c99dc6ac46ac2d4c66
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-28 11:16:50 +0200

    Dev: précision du code de réponse de l'exception

--- src/main/java/com/campus/cars/exceptions/carNotFoundException.java
+++ src/main/java/com/campus/cars/exceptions/carNotFoundException.java
@@ -1,5 +1,9 @@
 package com.campus.cars.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class carNotFoundException extends RuntimeException {
 	
 	public carNotFoundException(String s) {
